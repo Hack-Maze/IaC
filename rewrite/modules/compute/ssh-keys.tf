d8aecba1ad867dfc6401033c259ad40cfc2cc567
Mostafa-ewida <meewida0@gmail.com>
2024-01-07 19:50:17 +0200

    trying it out

--- rewrite/modules/compute/ssh-keys.tf
+++ rewrite/modules/compute/ssh-keys.tf
@@ -96,27 +96,52 @@ resource "local_file" "worker2_private_key" {
                 local_file.worker1_private_key,
                 local_file.worker2_private_key]
 
+
+  provisioner "file" {
+    source      = "/tmp/.ssh/control_public_key.pub"
+    destination = "/home/hackmaze-user/.ssh/control_public_key.pub"
+    }
+
   provisioner "file" {
     source      = "/tmp/.ssh/control_private_key.pem"
     destination = "/home/hackmaze-user/.ssh/control_private_key.pem"
     }
+
+
+
+  provisioner "file" {
+  source      = "/tmp/.ssh/worker1_public_key.pub"
+  destination = "/home/hackmaze-user/.ssh/worker1_public_key.pub"
+  }
 
   provisioner "file" {
     source      = "/tmp/.ssh/worker1_private_key.pem"
     destination = "/home/hackmaze-user/.ssh/worker1_private_key.pem"
     }
 
-    provisioner "file" {
-      source      = "/tmp/.ssh/worker2_private_key.pem"
-      destination = "/home/hackmaze-user/.ssh/worker2_private_key.pem"
-      }
 
-    connection {
-      type        = "ssh"
-      user        = "hackmaze-user"
-      private_key = local_file.jump_private_key.content // replace with the correct path to your private key
-      host        = var.jump_public_ip // replace with the public IP of your jump server
-      timeout    = "20m"
+  provisioner "file" {
+  source      = "/tmp/.ssh/worker2_public_key.pub"
+  destination = "/home/hackmaze-user/.ssh/worker2_public_key.pub"
+  }
+
+  provisioner "file" {
+    source      = "/tmp/.ssh/worker2_private_key.pem"
+    destination = "/home/hackmaze-user/.ssh/worker2_private_key.pem"
+    }
+
+
+
+  provisioner "remote-exec" {
+    inline = ["chmod 600 /home/hackmaze-user/.ssh/*"]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "hackmaze-user"
+    private_key = local_file.jump_private_key.content // replace with the correct path to your private key
+    host        = var.jump_public_ip // replace with the public IP of your jump server
+    timeout    = "20m"
 
   }
 
