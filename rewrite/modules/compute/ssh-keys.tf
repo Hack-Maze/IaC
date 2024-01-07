fb383f487e27193e5369484e2389088b18c26613
Mostafa-ewida <meewida0@gmail.com>
2024-01-07 17:44:35 +0200

    trying it out

--- rewrite/modules/compute/ssh-keys.tf
+++ rewrite/modules/compute/ssh-keys.tf
@@ -101,7 +101,7 @@ resource "local_file" "worker2_private_key" {
     }
 
   provisioner "file" {
-    source      = "/tmp/.ssh/worker2_private_key.pem.pem"
+    source      = "/tmp/.ssh/worker2_private_key.pem"
     destination = "/home/hackmaze-user/.ssh/"
     }
 
