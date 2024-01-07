84beacba462f0ef00248e66472f74849f9d97f16
Mostafa-ewida <meewida0@gmail.com>
2024-01-07 18:37:28 +0200

    trying it out

--- rewrite/modules/compute/ssh-keys.tf
+++ rewrite/modules/compute/ssh-keys.tf
@@ -97,17 +97,17 @@ resource "local_file" "worker2_private_key" {
 
   provisioner "file" {
     source      = "/tmp/.ssh/control_private_key.pem"
-    destination = "/home/hackmaze-user/.ssh/"
+    destination = "/home/hackmaze-user/.ssh/control_private_key.pem"
     }
 
   provisioner "file" {
-    source      = "/tmp/.ssh/worker2_private_key.pem"
-    destination = "/home/hackmaze-user/.ssh/"
+    source      = "/tmp/.ssh/worker1_private_key.pem"
+    destination = "/home/hackmaze-user/.ssh/worker1_private_key.pem"
     }
 
     provisioner "file" {
-      source      = "/tmp/.ssh/worker1_private_key.pem"
-      destination = "/home/hackmaze-user/.ssh/"
+      source      = "/tmp/.ssh/worker2_private_key.pem"
+      destination = "/home/hackmaze-user/.ssh/worker2_private_key.pem"
       }
 
     connection {
