07006791ad26b43bc93e0d1639df5d98533d97d8
Mostafa-ewida <meewida0@gmail.com>
2024-01-07 18:05:19 +0200

    trying it out

--- rewrite/modules/compute/ssh-keys.tf
+++ rewrite/modules/compute/ssh-keys.tf
@@ -95,29 +95,26 @@ resource "local_file" "worker2_private_key" {
                 local_file.worker1_private_key,
                 local_file.worker2_private_key]
 
-  provisioner "file" {
-    source      = "/tmp/.ssh/control_private_key.pem"
-    destination = "/home/hackmaze-user/.ssh/"
+    provisioner "local-exec" {
+      command = "scp -i  /tmp/.ssh/jump_private_key.pem /tmp/.ssh/control_private_key.pem ec2-user@${var.control_static_private_ip}:/home/hackmaze-user/.ssh/"
     }
 
-  provisioner "file" {
-    source      = "/tmp/.ssh/worker2_private_key.pem"
-    destination = "/home/hackmaze-user/.ssh/"
+   provisioner "local-exec" {
+      command = "scp -i  /tmp/.ssh/jump_private_key.pem /tmp/.ssh/worker1_private_key.pem ec2-user@${var.worker1_static_private_ip}:/home/hackmaze-user/.ssh/"
     }
 
-  provisioner "file" {
-    source      = "/tmp/.ssh/worker1_private_key.pem"
-    destination = "/home/hackmaze-user/.ssh/"
+    provisioner "local-exec" {
+      command = "scp -i  /tmp/.ssh/jump_private_key.pem /tmp/.ssh/worker2_private_key.pem ec2-user@${var.worker2_static_private_ip}:/home/hackmaze-user/.ssh/"
     }
 
-    connection {
-      type        = "ssh"
-      user        = "hackmaze-user"
-      private_key = local_file.jump_private_key.content // replace with the correct path to your private key
-      host        = var.jump_public_ip // replace with the public IP of your jump server
-      timeout    = "20m"
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "hackmaze-user"
+  #     private_key = local_file.jump_private_key.content // replace with the correct path to your private key
+  #     host        = var.jump_public_ip // replace with the public IP of your jump server
+  #     timeout    = "20m"
 
-  }
+  # }
 
   }
 
