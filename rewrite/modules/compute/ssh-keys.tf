533a5d9e73fbe4a0d2a3b64297830cce4c4e6840
Mostafa-ewida <meewida0@gmail.com>
2024-01-09 22:55:39 +0200

    fixing rules

--- rewrite/modules/compute/ssh-keys.tf
+++ rewrite/modules/compute/ssh-keys.tf
@@ -96,33 +96,17 @@ resource "local_file" "worker2_private_key" {
                 local_file.worker2_private_key]
 
 
-  provisioner "file" {
-    source      = "/tmp/.ssh/control_public_key.pub"
-    destination = "/home/hackmaze-user/.ssh/control_public_key.pub"
-    }
+
 
   provisioner "file" {
     source      = "/tmp/.ssh/control_private_key.pem"
     destination = "/home/hackmaze-user/.ssh/control_private_key.pem"
     }
 
-
-
-  provisioner "file" {
-  source      = "/tmp/.ssh/worker1_public_key.pub"
-  destination = "/home/hackmaze-user/.ssh/worker1_public_key.pub"
-  }
-
   provisioner "file" {
     source      = "/tmp/.ssh/worker1_private_key.pem"
     destination = "/home/hackmaze-user/.ssh/worker1_private_key.pem"
     }
-
-
-  provisioner "file" {
-  source      = "/tmp/.ssh/worker2_public_key.pub"
-  destination = "/home/hackmaze-user/.ssh/worker2_public_key.pub"
-  }
 
   provisioner "file" {
     source      = "/tmp/.ssh/worker2_private_key.pem"
@@ -130,13 +114,10 @@ resource "local_file" "worker2_private_key" {
     }
 
 
+    
 
   provisioner "remote-exec" {
     inline = ["chmod 600 /home/hackmaze-user/.ssh/*.pem"]
-  }
-
-  provisioner "remote-exec" {
-    inline = ["chmod 644 /home/hackmaze-user/.ssh/*.pub"]
   }
 
   connection {
--- rewrite/modules/security/worker-sg.tf
+++ rewrite/modules/security/worker-sg.tf
@@ -51,12 +51,47 @@ resource "azurerm_network_security_rule" "ssh_worker_rule" {
   network_security_group_name = azurerm_network_security_group.worker-sg-01.name
 }
 
+
+
+
+resource "azurerm_network_security_rule" "fannel8285_rule" {
+  name                        = "Allowfannel8285"
+  priority                    = 103
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  destination_port_range      = "8285"
+  source_port_range           = "*"
+  source_address_prefix       = var.hackmaze_vnet_address_range[0]
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rc-name
+  network_security_group_name = azurerm_network_security_group.worker-sg-01.name
+}
+
+
+resource "azurerm_network_security_rule" "fannel8472_rule" {
+  name                        = "Allowfannel8472"
+  priority                    = 104
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  destination_port_range      = "8472"
+  source_port_range           = "*"
+  source_address_prefix       = var.hackmaze_vnet_address_range[0]
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rc-name
+  network_security_group_name = azurerm_network_security_group.worker-sg-01.name
+}
+
+
+
+
 resource "azurerm_network_interface_security_group_association" "hackmaze-worker1-sga-01" {
   network_interface_id      = var.worker1_nic_id
   network_security_group_id = azurerm_network_security_group.worker-sg-01.id
 }
 
-resource "azurerm_network_interface_security_group_association" "hackmaze-worker-sga-01" {
+resource "azurerm_network_interface_security_group_association" "hackmaze-worker2-sga-01" {
   network_interface_id      = var.worker2_nic_id
   network_security_group_id = azurerm_network_security_group.worker-sg-01.id
 }
