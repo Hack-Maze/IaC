362a068b6df53f2e55069f032ccbd66b181f7c06
Mostafa-ewida <meewida0@gmail.com>
2024-01-11 13:38:06 +0200

    ssh-transfer

--- rewrite/modules/network/public_ips.tf
+++ rewrite/modules/network/public_ips.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "jump_server_ip" {
  name                = "jump-server-ip"
  location            = var.rc-location
  resource_group_name = var.rc-name
- allocation_method   = "Dynamic"
+ allocation_method   = "Static"
  domain_name_label   = "${var.dns_name}"
  tags                = var.rc-tags
 }
