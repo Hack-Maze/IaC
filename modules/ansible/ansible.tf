2911c62dc195f6a4806c963115318cfe2dcc54f6
Mostafa-ewida <meewida0@gmail.com>
2024-01-17 00:20:28 +0200

    making ansible setup cluster automatically

--- modules/ansible/ansible.tf
+++ modules/ansible/ansible.tf
@@ -62,7 +62,7 @@ resource "null_resource" "setup_ansible" {
 
 
 resource "null_resource" "setup_cluster" {
-
+  depends_on = [null_resource.setup_ansible]
   provisioner "remote-exec" {
    inline = [
     "ansible-playbook -i ~/ansible/inventory.txt ~/ansible/k8s-setup/*.yml"
