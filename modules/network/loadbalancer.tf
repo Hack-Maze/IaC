2ef4d1400f4edfd4542e65bed1c1619a53432609
Mostafa-ewida <meewida0@gmail.com>
2024-01-18 20:21:36 +0200

    outbound rule  hotfix

--- modules/network/loadbalancer.tf
+++ modules/network/loadbalancer.tf
@@ -59,14 +59,13 @@ resource "azurerm_lb_rule" "httprule" {
   frontend_ip_configuration_name = "LB-IP"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb-backend-pool.id]
   probe_id                       = azurerm_lb_probe.httpPorbe.id
-  disable_outbound_snat          = true
 } 
 
 
 resource "azurerm_lb_outbound_rule" "Outbound" {
   name                    = "OutboundRule"
   loadbalancer_id         = azurerm_lb.hm-lb.id
-  protocol                = "Tcp"
+  protocol                = "All"
   backend_address_pool_id = azurerm_lb_backend_address_pool.lb-backend-pool.id
   frontend_ip_configuration {
     name = "LB-IP"
